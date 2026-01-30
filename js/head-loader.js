df66a273e1099eaa86644ed7a8d6b1083cac2669
Bargas Santiago Dario <santidariobargas@hotmail.com>
2026-01-29 21:16:10 -0300

    .

--- js/head-loader.js
+++ js/head-loader.js
@@ -2,8 +2,6 @@
 (function() {
     const head = document.head;
     const resources = `
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="lib/flaticon/font/flaticon.css">
         <link rel="stylesheet" href="css/fontello.css">
